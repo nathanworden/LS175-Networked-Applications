d542682996cb65569e2b20206b8e46efa1b3bf22
Nathan Worden <worden.nathan@gmail.com>
2019-11-25 09:40:29 -0800

    a simple echo server

--- 02.handling_requests_manually/03.a_simple_echo_server.rb
+++ 02.handling_requests_manually/03.a_simple_echo_server.rb
@@ -1,16 +1,41 @@
 require "socket"
 
+def parse_request(request_line)
+  http_method, path_and_params, http = request_line.split(" ")
+  path, params = path_and_params.split("?")
+
+  params = params.split("&").each_with_object({}) do |pair, hash|
+    key, value = pair.split("=")
+    hash[key] = value
+  end
+  [http_method, path, params]
+end
+
 server = TCPServer.new("localhost", 3003)
 loop do
   client = server.accept
-
   client.puts "HTTP/1.1 200 OK\r\n\r\n"
 
   request_line = client.gets
+  next if !request_line || request_line =~ /favicon/
   puts request_line
 
+  http_method, path, params = parse_request(request_line)
+  
   client.puts request_line
-  client.puts rand(6) + 1
+  client.puts http_method
+  client.puts path
+  client.puts params
+
+  rolls = params["rolls"].to_i
+  sides = params["sides"].to_i
+
+  rolls.times do 
+    roll = rand(sides) + 1
+    client.puts roll
+  end
+
+  # client.puts rand(6) + 1
 
   client.close
 end
