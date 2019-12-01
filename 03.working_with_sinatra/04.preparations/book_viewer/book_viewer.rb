555da76066b8de15204f35e55b10061c74cbaa49
Nathan Worden <worden.nathan@gmail.com>
2019-12-01 07:19:42 -0800

    sinatra code challenge

--- 03.working_with_sinatra/04.preparations/book_viewer/book_viewer.rb
+++ 03.working_with_sinatra/04.preparations/book_viewer/book_viewer.rb
@@ -4,5 +4,13 @@ require "tilt/erubis"
 
 get "/" do
   @title = "Los Tiburones de Senior Sherlock"
+  @contents = File.readlines("data/toc.txt")
   erb :home
+end
+
+get "/chapters/1" do
+  @title = "Chapter 1"
+  @contents = File.readlines("data/toc.txt")
+  @chapter = File.read("data/chp1.txt")
+  erb :chapter
 end
