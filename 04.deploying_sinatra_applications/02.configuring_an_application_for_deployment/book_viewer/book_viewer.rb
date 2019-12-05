17bed94e0418c6342f452be9f1f97ff26cf894f9
Nathan Worden <worden.nathan@gmail.com>
2019-12-05 14:20:19 -0800

    Procfile

---
+++ 04.deploying_sinatra_applications/02.configuring_an_application_for_deployment/book_viewer/book_viewer.rb
@@ -0,0 +1,90 @@
+require "sinatra"
+require "sinatra/reloader" if development?
+require "tilt/erubis"
+
+before do
+  @contents = File.readlines("data/toc.txt")
+end
+
+get "/" do
+  @title = "The Adventures of Sherlock Holmes"
+
+  erb :home
+end
+
+get "/chapters/:number" do
+  number = params[:number].to_i
+  chapter_name = @contents[number - 1]
+
+  redirect "/" unless (1..@contents.size).cover?(number)
+
+  @title = "Chapter #{number}: #{chapter_name}"
+  @chapter = File.read("data/chp#{number}.txt")
+
+  erb :chapter
+end
+
+
+
+# Calls the block for each chapter, passing that chapter's number, name, and
+# contents.
+def each_chapter
+  @contents.each_with_index do |name, index|
+    number = index + 1
+    contents = File.read("data/chp#{number}.txt")
+    yield(number, name, contents)
+  end
+end
+
+# This method returns an Array of Hashes representing chapters that match the
+# specified query. Each Hash contain values for its :name and :number keys.
+def chapters_matching(query)
+  results = []
+
+  return results unless query
+
+  each_chapter do |number, name, contents|
+    matches = {}
+    contents.split("\n\n").each_with_index do |paragraph, index|
+      matches[index] = paragraph if paragraph.include?(query)
+    end
+    results << {number: number, name: name, paragraphs: matches} if matches.any?
+  end
+
+  results
+end
+
+get "/search" do
+  @results = chapters_matching(params[:query])
+  erb :search
+end
+
+helpers do
+  def in_paragraphs(text)
+    text.split("\n\n").each_with_index.map do |line, index|
+      "<p id=paragraph#{index}>#{line}</p>"
+    end.join
+  end
+
+  def bold_param(txt)
+    start_idx = txt.index(params[:query])
+    end_idx = start_idx + params[:query].length
+    word_idx = txt[start_idx...end_idx]
+    before_word = txt[0...start_idx]
+    after_word = txt[end_idx..-1]
+    "#{before_word}<strong>#{word_idx}</strong>#{after_word}"
+  end
+end
+
+get "/show/:name" do
+  params[:name]
+end
+
+not_found do
+  redirect "/"
+end
+
+
+
+
+
