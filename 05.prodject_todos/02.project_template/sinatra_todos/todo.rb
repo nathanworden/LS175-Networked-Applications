14d20c9bb175b7e30d3af9b2d7a666c100f50eac
Nathan Worden <worden.nathan@gmail.com>
2019-12-10 11:44:44 -0800

    todo list sinatra project view templates

--- 05.prodject_todos/02.project_template/sinatra_todos/todo.rb
+++ 05.prodject_todos/02.project_template/sinatra_todos/todo.rb
@@ -1,6 +1,8 @@
 require "sinatra"
 require "sinatra/reloader"
+require "sinatra/content_for"
 require "tilt/erubis"
+
 
 configure do
   enable :sessions
@@ -11,7 +13,7 @@ before do
   session[:lists] ||= []
 end
 
-get "/" do 
+get "/" do
   redirect "/lists"
 end
 
@@ -21,8 +23,6 @@ end
 # GET /lists/1      -> view a single list
 # GET /users
 # GET /users/1
-
-
 
 # View list of lists
 get "/lists" do
@@ -40,12 +40,59 @@ get "/lists/delete" do
   redirect "/lists"
 end
 
-# Create a new list
-post "/lists" do
-  session[:lists] <<{name: params[:list_name], todos:[]}
-  redirect "/lists"
+# Return an error message if the name is invalid. Return nil if name is valid.
+def error_for_list_name(name)
+  if !(1..100).cover? name.size
+    "List name has to be between 1 and 100 characters."
+  elsif session[:lists].any? { |list| list[:name] == name }
+    "List name must be unique."
+  end
 end
 
+# Create a new list
+post "/lists" do
+  list_name = params[:list_name].strip
+
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
+    erb :new_list, layout: :layout
+  else
+    session[:lists] << { name: list_name, todos: [] }
+    session[:success] = "The list has been created."
+    redirect "/lists"
+  end
+end
+
+get "/lists/:id" do
+  id = params[:id].to_i
+  @list = session[:lists][id]
+  erb :list, layout: :layout
+end
+
+# Edit an existing todo list
+get "/lists/:id/edit" do
+  id = params[:id].to_i
+  @list = session[:lists][id]
+  erb :edit_list, layout: :layout
+end
+
+# Update an existing todo list
+post "/lists/:id" do
+  list_name = params[:list_name].strip
+  id = params[:id].to_i
+  @list = session[:lists][id]
+
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
+    erb :edit_list, layout: :layout
+  else
+    @list[:name] = list_name
+    session[:success] = "The list has been updated."
+    redirect "/lists/#{id}"
+  end
+end
 
 
 
