ae801a3e4898ed4e480e60385d39655d3e3a40bd
Nathan Worden <worden.nathan@gmail.com>
2019-12-22 12:45:00 -0800

    adding tests

---
+++ 08.project_file_based_cms/02.getting_started/test/cms_test.rb
@@ -0,0 +1,25 @@
+ENV["RACK_ENV"] = "test"
+
+require "minitest/autorun"
+require "rack/test"
+
+require_relative "../cms"
+
+class CMSTest < Minitest::Test
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def test_index
+    get "/"
+    assert_equal 200, last_response.status
+    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
+    assert_equal "Hello, world!", last_response.body
+  end
+
+  def test_history
+    get "/history.txt"
+  end
+end
