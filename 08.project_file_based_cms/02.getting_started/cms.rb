212064b523a55b08233bf2987d36b0aa925731c6
Nathan Worden <worden.nathan@gmail.com>
2019-12-23 05:49:21 -0800

    updating Gemfile and cms project

--- 08.project_file_based_cms/02.getting_started/cms.rb
+++ 08.project_file_based_cms/02.getting_started/cms.rb
@@ -4,6 +4,11 @@ require "sinatra/reloader" if development?
 require "sinatra/content_for"
 require "tilt/erubis"
 
+configure do
+  enable :sessions
+  set :session_secret, 'super secret'
+end
+
 root = File.expand_path("..", __FILE__)
 
 get "/" do
@@ -16,6 +21,11 @@ end
 get "/:filename" do
   file_path = root + "/data/" + params[:filename]
 
-  headers["Content-Type"] = "text/plain"
-  File.read(file_path)
+  if File.file?(file_path)
+    headers["Content-Type"] = "text/plain"
+    File.read(file_path)
+  else
+    session[:message] = "#{params[:filename]} does not exist."
+    redirect "/"
+  end
 end
--- 08.project_file_based_cms/02.getting_started/test/cms_test.rb
+++ 08.project_file_based_cms/02.getting_started/test/cms_test.rb
@@ -16,10 +16,33 @@ class CMSTest < Minitest::Test
     get "/"
     assert_equal 200, last_response.status
     assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
-    assert_equal "Hello, world!", last_response.body
+    assert_includes last_response.body, "about.txt"
+    assert_includes last_response.body, "changes.txt"
+    assert_includes last_response.body, "history.txt"
   end
 
   def test_history
     get "/history.txt"
+
+    assert_equal 200, last_response.status
+    assert_equal "text/plain", last_response["Content-Type"]
+    assert_includes last_response.body, "Ruby 0.95 released"
+  end
+
+  def test_document_not_found
+    get "/notafile.ext" # Attempt to access a nonexistent file
+
+    assert_equal 302, last_response.status # Assert that the user was redirected
+    get last_response["Location"] # Request the page that the user was redircted to
+
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "notafile.ext does not exist"
+
+    get "/" # Reload the page
+    refute_includes last_response.body, "notafile.ext does not exist" # Assert that our message has been removed
   end
 end
+
+
+
+
