431d27a951622f5f54741c892fd8a18d93e90d3c
Nathan Worden <worden.nathan@gmail.com>
2019-12-31 08:27:40 -0800

    project_file_based_cms

--- 08.project_file_based_cms/21.summary/170_cms_final_code/cms.rb
+++ 08.project_file_based_cms/21.summary/170_cms_final_code/cms.rb
@@ -6,11 +6,6 @@ require "tilt/erubis"
 require "redcarpet"
 require "bcrypt"
 
-require 'rouge'
-require "rouge/plugins/redcarpet"
-
-require "sinatra/content_for"
-
 configure do
   enable :sessions
   set :session_secret, 'super secret'
@@ -45,20 +40,9 @@ def valid_credentials?(username, password)
 end
 
 def render_markdown(text)
-  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, fenced_code_blocks: true, highlight: true, underline: true, quote: true, prettify: true)
+  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
   markdown.render(text)
 end
-
-# def load_file_content(path)
-#   content = File.read(path)
-#   case File.extname(path)
-#   when ".md"
-#     erb render_markdown(content)
-#   else
-#     headers["Content-Type"] = "text/plain"
-#     content
-#   end
-# end
 
 def load_file_content(path)
   content = File.read(path)
@@ -82,49 +66,12 @@ def require_signed_in_user
   end
 end
 
-# A Note About Security. Example:
-# get "/view" do
-#   file_path = File.join(data_path, params[:filename])
-
-#   if File.exist?(file_path)
-#     load_file_content(file_path)
-#   else
-#     session[:message] = "#{params[:filename]} does not exist."
-#     redirect "/"
-#   end
-# end
-
 get "/" do
   pattern = File.join(data_path, "*")
-
   @files = Dir.glob(pattern).map do |path|
     File.basename(path)
   end
   erb :index
-end
-
-get "/users/signin" do
-  erb :signin
-end
-
-post "/users/signin" do
-  username = params[:username]
-
-  if valid_credentials?(username, params[:password])
-    session[:username] = username
-    session[:message] = "Welcome!"
-    redirect "/"
-  else
-    session[:message] = "Invalid credentials"
-    status 422
-    erb :signin
-  end
-end
-
-post "/users/signout" do
-  session.delete(:username)
-  session[:message] = "You have been signed out."
-  redirect "/"
 end
 
 get "/new" do
@@ -143,14 +90,6 @@ post "/create" do
     status 422
     erb :new
   else
-    if filename[-4..-1] == ".txt"
-      filename
-    elsif filename[-3..-1] == ".md"
-      filename
-    else
-      filename += ".txt"
-    end
-    
     file_path = File.join(data_path, filename)
 
     File.write(file_path, "")
@@ -195,7 +134,7 @@ end
 
 post "/:filename/delete" do
   require_signed_in_user
-  
+
   file_path = File.join(data_path, params[:filename])
 
   File.delete(file_path)
@@ -204,14 +143,26 @@ post "/:filename/delete" do
   redirect "/"
 end
 
+get "/users/signin" do
+  erb :signin
+end
 
+post "/users/signin" do
+  username = params[:username]
 
+  if valid_credentials?(username, params[:password])
+    session[:username] = username
+    session[:message] = "Welcome!"
+    redirect "/"
+  else
+    session[:message] = "Invalid credentials"
+    status 422
+    erb :signin
+  end
+end
 
-
-
-
-
-
-
-
-
+post "/users/signout" do
+  session.delete(:username)
+  session[:message] = "You have been signed out."
+  redirect "/"
+end
